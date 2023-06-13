f89602eebc0efc737d9d51ed1a9dac082ed9de3d
Ildar-Ilyasov <ildar.ilyasov123@yandex.com>
2023-06-13 08:36:57 +0300

    init commit

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,9 @@
 enum ClothesSize {
     XXS(32), XS(34), S(36), M(38), L(40);
-
     private final int euroSize;
-
     ClothesSize(int euroSize) {
         this.euroSize = euroSize;
     }
-
     public String getDescription() {
         if (this == XXS) {
             return "Детский размер";
@@ -14,109 +11,85 @@ enum ClothesSize {
             return "Взрослый размер";
         }
     }
-
     public int getEuroSize() {
-
         return euroSize;
     }
 }
-
 interface MaleClothes {
     void dressMan();
 }
-
 interface FemaleClothes {
     void dressWoman();
 }
-
 abstract class Clothes {
     private ClothesSize size;
     private double cost;
     private String color;
-
     Clothes(ClothesSize size, double cost, String color) {
         this.size = size;
         this.cost = cost;
         this.color = color;
     }
-
     public ClothesSize getSize() {
-
         return size;
     }
-
     public double getCost() {
-
         return cost;
     }
-
     public String getColor() {
-
         return color;
     }
 }
-
 class TShirt extends Clothes implements MaleClothes, FemaleClothes {
     TShirt(ClothesSize size, double cost, String color) {
-
         super(size, cost, color);
     }
-
     @Override
     public void dressMan() {
         System.out.println("Мужчина одел футболку размера " + getSize().name() +
                 ", цвета " + getColor() + ", стоимостью " + getCost() + ".");
     }
-
     @Override
     public void dressWoman() {
         System.out.println("Женщина одела футболку размера " + getSize().name() +
                 ", цвета " + getColor() + ", стоимостью " + getCost() + ".");
     }
 }
-
 class Pants extends Clothes implements MaleClothes, FemaleClothes {
     Pants(ClothesSize size, double cost, String color) {
         super(size, cost, color);
     }
-
     @Override
     public void dressMan() {
         System.out.println("Мужчина одел штаны размера " + getSize().name() +
                 ", цвета " + getColor() + ", стоимостью " + getCost() + ".");
     }
-
     @Override
     public void dressWoman() {
         System.out.println("Женщина одела штаны размера " + getSize().name() +
                 ", цвета " + getColor() + ", стоимостью " + getCost() + ".");
     }
 }
-
 class Skirt extends Clothes implements FemaleClothes {
     Skirt(ClothesSize size, double cost, String color) {
         super(size, cost, color);
     }
-
     @Override
     public void dressWoman() {
         System.out.println("Женщина одела юбку размера " + getSize().name() +
                 ", цвета " + getColor() + ", стоимостью " + getCost() + ".");
     }
 }
-
 class Tie extends Clothes implements MaleClothes {
     Tie(ClothesSize size, double cost, String color) {
         super(size, cost, color);
     }
-
     @Override
     public void dressMan() {
         System.out.println("Мужчина надел галстук размера " + getSize().name() +
                 ", цвета " + getColor() + ", стоимостью " + getCost() + ".");
     }
 }
-
 class Atelier {
     public void dressWoman(Clothes[] clothes) {
         System.out.println("Женская одежда:");
@@ -126,7 +99,6 @@ class Atelier {
             }
         }
     }
-
     public void dressMan(Clothes[] clothes) {
         System.out.println("Мужская одежда:");
         for (Clothes c : clothes) {
@@ -139,12 +111,11 @@ class Atelier {
 public class Main {
     public static void main(String[] args) {
         Clothes[] clothes = {
-                new TShirt(ClothesSize.XXS, 15.99, "белый"),
+                new TShirt(ClothesSize.XXS, 19.99, "белый"),
                 new Pants(ClothesSize.M, 29.99, "черный"),
-                new Skirt(ClothesSize.S, 25.50, "красный"),
-                new Tie(ClothesSize.L, 10.49, "синий")
+                new Skirt(ClothesSize.S, 20.99, "красный"),
+                new Tie(ClothesSize.L, 12.99, "синий")
         };
-
         Atelier atelier = new Atelier();
         atelier.dressWoman(clothes);
         atelier.dressMan(clothes);
